85a122e9d72799f75f5f1a59c0f79c26ac5451b4
tmpITK <torok.mark.patrik@gmail.com>
2020-08-10 19:19:52 +0200

    Add dictionary, reading for motnly and daily data, and update db accordingly

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,32 @@ import pandas as pd
 import numpy as np
 import glob
 
-def read_data(path):
+dictionary = {'Előzmények': 'date',
+              'Testsúly (kg)' : 'weight',
+              'Testmozgás (kcal)': 'burnt_calories',
+              'Étel (kcal)': 'calorie_intake',
+              'Zsír (g)' : 'fat',
+              'Telített zsírok (g)' : 'saturated_fat',
+              'Szénhídrátok (g)' : 'carbonhydrate',
+              'Cukrok (g)' : 'sugar',
+              'Fehérje (g)' : 'protein',
+              'Élelmi rost (g)' : 'fiber',
+              'Só (g)' : 'salt'}
+
+def read_daily_data(path : str) -> pd.DataFrame:
+    # TODO lot of distillation is needed for daily data
     temp_list = []
     all_files = glob.glob(path + r'\*.csv')
+    for file_name in all_files:
+        df = pd.read_csv(file_name)
+        temp_list.append(df.T)
+
+    return pd.concat(temp_list, axis=0)
+
+def read_monthly_data(path : str) -> pd.DataFrame:
+    temp_list = []
+    all_files = glob.glob(path + r'\*.csv')
+
     for file_name in all_files:
         df = pd.read_csv(file_name)
         temp_list.append(df.T)
