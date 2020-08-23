b9a57e5f88b77f415add98667000cc0543ba732b
tmpITK <torok.mark.patrik@gmail.com>
2020-08-23 20:01:08 +0200

    Add plotter usage or more like tests

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from utils.constants import PATH_DB
 from data_handlers.MonthlyDataProcessor import MonthlyDataProcessor
+from plotting.HealthPlotter import MonthlyPlotter
 import pickle
 
 if __name__ == '__main__':
     dp = MonthlyDataProcessor(PATH_DB)
     clean_data = dp.process_database()
+    mp = MonthlyPlotter()
+
+    mp.plot_column(clean_data[0], columnToPlot='calorie_intake')
+    mp.plot_full_data(clean_data)
+
     #testing
     with open(r'D:\Python_projects\health_analyzer\db\monthly\clean\clean.pickle', 'rb') as f:
         result = pickle.load(f)
